b63518231cf8acce0cfb1fb8507c89fbf826c832
AdityaMukherj33 <adityamukherjee2004@gmail.com>
2024-12-24 14:02:44 +0530

    Update module.py

--- module.py
+++ module.py
@@ -1,3 +1,6 @@
 import math as m
-print(m.pi)
-m.sqrt(4)
+from random import *
+print(uniform(1,10))
+l=[2,3,4,5,6,7,8,8,9,6,4,3,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,12,1,21,212,2]
+for _ in range(20):
+    print(choice(l))
